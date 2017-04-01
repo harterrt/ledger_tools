79a50906776ecfcb94623cff82b5b494cd7505bf
Ryan Harter <harterrt@gmail.com>
2017-04-01 14:57:10 -0400

    Add stub functions for finding new transactions
    Needs testing and some debugging, note that amount types do not agree between
    mint and ledger

--- ledger_tools/data_actions.py
+++ ledger_tools/data_actions.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from ledger_tools import mint, ledger
 
 
@@ -7,3 +8,48 @@ def render_new_trans(mint_file, ledger_file):
     ledger_trans = ledger.read_ledger_trans(ledger_file)
 
     return render(diff(mint_trans, ledger_trans))
+
+
+TransactionKey = namedtuple(
+    'TransactionKey',
+    ['date', 'amount', 'description']
+)
+
+
+def key_mint_tran(mint_tran):
+    return (
+        TransactionKey(
+            mint_tran['date'],
+            mint_tran['amount'],
+            mint_tran['original description']
+        ),
+        mint_tran
+    )
+
+
+def simplify_ledger_tran(ledger_tran):
+    return TransactionKey(
+        ledger_tran['date'],
+        ledger_tran['amount'],
+        ledger_tran['payee']
+    )
+
+
+def is_recorded(keyed_mint_tran, ledger_trans):
+    return keyed_mint_tran[0] in ledger_trans
+
+
+def trans_filter(ledger_trans):
+    """Return a filter function for a given set of ledger transactions
+    
+    This implementation is naive, but this format makes it easy to refactor
+    to a statistical filter if we have performance issues"""
+    simple_ledger_trans = map(simplify_ledger_tran, ledger_trans)
+    return lambda mint_tran: is_recorded(mint_tran, simple_ledger_trans)
+
+
+
+def find_new(mint_trans, ledger_trans):
+    """Finds mint trans not yet included in a list of ledger trans"""
+    keyed_trans = map(key_mint_tran, mint_trans)
+    new_trans = filter(trans_filter(ledger_trans), keyed_trans)
