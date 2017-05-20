ef6f8cbdf0479f47aeebd2425a0a9f657965b472
Ryan Harter <harterrt@gmail.com>
2017-05-20 15:06:27 -0400

    Prototype tests for categorize
    Everything passes now, but it doesn't actually do anything. refactor
    this test to handle actual categorization and start hacking!!!

--- tests/data/new_transactions.py
+++ tests/data/new_transactions.py
@@ -14,3 +14,40 @@ new_transactions = [
         'transaction type': 'debit'
     }
 ]
+
+
+many_new_transactions = [
+    {
+        'account name': 'CREDIT CARD',
+        'amount': '10.00',
+        'category': 'Gas & Fuel',
+        'date': datetime.date(2011, 4, 10),
+        'description': 'New Tran Description',
+        'labels': '',
+        'notes': '',
+        'original description': 'Orig desc',
+        'transaction type': 'debit'
+    },
+    {
+        'account name': 'CREDIT CARD',
+        'amount': '50.00',
+        'category': 'Gas & Fuel',
+        'date': datetime.date(2011, 4, 10),
+        'description': 'New Tran Description',
+        'labels': '',
+        'notes': '',
+        'original description': 'Orig desc',
+        'transaction type': 'debit'
+    },
+    {
+        'account name': 'CREDIT CARD',
+        'amount': '67.00',
+        'category': 'Gas & Fuel',
+        'date': datetime.date(2011, 4, 10),
+        'description': 'New Tran Description',
+        'labels': '',
+        'notes': '',
+        'original description': 'Orig desc',
+        'transaction type': 'debit'
+    }
+]
